5d97185bc4bfc5421a8dd5e2269d8d5ee919ebd7
Artem Ch <cherepahin-artem@ya.ru>
2020-11-15 20:23:39 +0300

    complete task 1

---
+++ task1_get_rating.sql
@@ -0,0 +1,7 @@
+create or replace view task1_get_rating as 
+select name_street
+      ,listagg(name_point, ',') within group(order by null) list_point
+      ,count(1) cnt_point
+  from list_street_with_wifi
+ group by name_street
+ order by cnt_point desc;
