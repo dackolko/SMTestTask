64cefb91ab64d05bd5515c2e24a2c0a56a710e58
Artem Ch <cherepahin-artem@ya.ru>
2020-11-21 19:53:24 +0300

    change create graph flows

--- graph_flows.sql
+++ graph_flows.sql
@@ -1,4 +1,4 @@
-create or replace view graph_flows as
+create or replace force view  graph_flows as
 with data_cross as
  (select s1.id_street from_street
         ,s2.id_street to_street
@@ -53,13 +53,13 @@ calc_xy as
         ,y3
         ,x4
         ,y4
-        ,x_cross --TODO почему минус при вертикальном отрезке
+        ,x_cross
         ,case x4 - x3
            when 0 then
             0
            else
             ((y3 - y4) * x_cross - (x3 * y4 - x4 * y3)) / (x4 - x3)
-         end y_cross --TODO почему при разных соединениях 1 и 2 или 2 и 1 разные координаты(может быть 0)
+         end y_cross
     from (select from_street
                 ,to_street
                 ,x1
@@ -73,67 +73,86 @@ calc_xy as
                 ,((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) /
                  ((y1 - y2) * (x4 - x3) - (y3 - y4) * (x2 - x1)) x_cross
             from d_flow)),
-get_point_cross as
- (select distinct t1.from_street
-                 ,t1.to_street
-                 ,t1.x1
-                 ,t1.y1
-                 ,t1.x2
-                 ,t1.y2
-                 ,t1.x3
-                 ,t1.y3
-                 ,t1.x4
-                 ,t1.y4
-                 ,abs(case t1.x_cross
-                        when 0 then
-                         t2.x_cross
-                        else
-                         t1.x_cross
-                      end) x_cross
-                 ,case t1.y_cross
-                    when 0 then
-                     t2.y_cross
-                    else
-                     t1.y_cross
-                  end y_cross
-    from calc_xy t1
-    left join calc_xy t2 on t1.from_street = t2.to_street)
---Выбираем все точки, от точки пересечения до конечной и от конечной к точке пересечения - это и есть ребра графа
-select from_street
-      ,to_street
-      ,x1 p1x
-      ,y1 p1y
-      ,x2 p2x
-      ,y2 p2y
-      ,utils_pkg.dist_between_points(x1, y1, x2, y2) cost_flow
-      ,case
-         when w.id_street is not null then
-          1
-         else
-          0
-       end wifi
-  from (select from_street
-              ,to_street
-              ,x1
-              ,y1
-              ,x_cross     x2
-              ,y_cross     y2
-          from get_point_cross f
+cross_point as
+ (select t.from_street
+        ,t.to_street
+        ,t.x1
+        ,t.y1
+        ,t.x2
+        ,t.y2
+        ,t.x3
+        ,t.y3
+        ,t.x4
+        ,t.y4
+        ,abs(t.x_cross) x_cross
+        ,case
+           when t.y_cross = 0 then
+            t1.y_cross
+           else
+            t.y_cross
+         end y_cross
+    from calc_xy t
+    join calc_xy t1 on t.from_street = t1.to_street
+                   and t.to_street = t1.from_street)
+--Получаем ребра графа
+,
+flows as
+ (select x
+        ,y
+        ,to_x
+        ,to_y
+        ,t.id_street
+        ,utils_pkg.dist_between_points(x, y, to_x, to_y) d
+        ,case
+           when t1.id_street is not null then
+            1
+           else
+            0
+         end wifi
+    from (select x
+                ,y
+                ,id_street
+                ,lead(x, 1) over(partition by id_street order by x, y) to_x
+                ,lead(y, 1) over(partition by id_street order by x, y) to_y
+            from (select first_x   x
+                        ,first_y   y
+                        ,id_street
+                    from streets
+                  union all
+                  select second_x  x
+                        ,second_y  y
+                        ,id_street
+                    from streets
+                  union all
+                  select distinct x_cross     x
+                                 ,y_cross     y
+                                 ,from_street id_street
+                    from cross_point)) t
+    left join list_street_with_wifi t1 on t.id_street = t1.id_street
+   where to_x is not null
+     and to_y is not null)
+--Добавляем обратное направление
+select distinct p1x
+               ,p1y
+               ,p2x
+               ,p2y
+               ,id_street
+               ,cost_flow
+               ,wifi
+  from (select x         p1x
+              ,y         p1y
+              ,to_x      p2x
+              ,to_y      p2y
+              ,id_street
+              ,d         cost_flow
+              ,wifi
+          from flows t
         union all
-        select from_street
-              ,to_street
-              ,x_cross     x1
-              ,y_cross     y1
-              ,x2
-              ,y2
-          from get_point_cross
-        union all
-        select to_street   from_street
-              ,from_street to_street
-              ,x_cross     x1
-              ,y_cross     y1
-              ,x1 x2
-              ,y1 y2
-          from get_point_cross)
-  left join (select distinct id_street
-               from list_street_with_wifi) w on from_street = w.id_street;
+        select to_x      p1x
+              ,to_y      p1y
+              ,x         p2x
+              ,y         p2y
+              ,id_street
+              ,d         cost_flow
+              ,wifi
+          from flows t);
